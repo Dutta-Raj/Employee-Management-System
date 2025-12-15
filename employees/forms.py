a38b3ccc0f3b136257ef954126b30ba896761e62
Rajdeep <rajdeepdutta104@gmail.com>
2025-12-16 00:16:23 +0530

    Fix models.py - restore EmployeeProfile and Attendance models

--- employees/forms.py
+++ employees/forms.py
@@ -1,23 +1,23 @@
-from django import forms 
-from django.contrib.auth.forms import UserCreationForm 
-from django.contrib.auth.models import User 
-from .models import EmployeeProfile 
- 
-class EmployeeRegistrationForm(UserCreationForm): 
-    email = forms.EmailField(required=True) 
- 
-    class Meta: 
-        model = User 
-        fields = ['username', 'email', 'password1', 'password2'] 
- 
-    def save(self, commit=True): 
-        user = super().save(commit=False) 
-        user.email = self.cleaned_data['email'] 
-        if commit: 
-            user.save() 
-        return user 
- 
-class EmployeeProfileForm(forms.ModelForm): 
-    class Meta: 
-        model = EmployeeProfile 
-        exclude = ['user'] 
+from django import forms  
+from django.contrib.auth.forms import UserCreationForm  
+from django.contrib.auth.models import User  
+from .models import EmployeeProfile  
+  
+class EmployeeRegistrationForm(UserCreationForm):  
+    email = forms.EmailField(required=True)  
+  
+    class Meta:  
+        model = User  
+        fields = ['username', 'email', 'password1', 'password2']  
+  
+    def save(self, commit=True):  
+        user = super().save(commit=False)  
+        user.email = self.cleaned_data['email']  
+        if commit:  
+            user.save()  
+        return user  
+  
+class EmployeeProfileForm(forms.ModelForm):  
+    class Meta:  
+        model = EmployeeProfile  
+        exclude = ['user']  
--- employees/models.py
+++ employees/models.py
@@ -1,25 +1,25 @@
-from django.db import models 
-from django.contrib.auth.models import User 
- 
-class EmployeeProfile(models.Model): 
-    user = models.OneToOneField(User, on_delete=models.CASCADE) 
-    employee_id = models.CharField(max_length=20, unique=True) 
-    department = models.CharField(max_length=100) 
-    position = models.CharField(max_length=100) 
-    phone = models.CharField(max_length=15) 
-    address = models.TextField() 
-    join_date = models.DateField(auto_now_add=True) 
-    profile_picture = models.ImageField(upload_to='profiles/', blank=True) 
- 
-    def __str__(self): 
-        return f'{self.user.username} - {self.employee_id}' 
- 
-class Attendance(models.Model): 
-    employee = models.ForeignKey(EmployeeProfile, on_delete=models.CASCADE) 
-    date = models.DateField(auto_now_add=True) 
-    time_in = models.TimeField() 
-    time_out = models.TimeField(null=True, blank=True) 
-    hours_worked = models.DecimalField(max_digits=5, decimal_places=2, default=0) 
- 
-    def __str__(self): 
-        return f'{self.employee} - {self.date}' 
+from django.db import models  
+from django.contrib.auth.models import User  
+  
+class EmployeeProfile(models.Model):  
+    user = models.OneToOneField(User, on_delete=models.CASCADE)  
+    employee_id = models.CharField(max_length=20, unique=True)  
+    department = models.CharField(max_length=100)  
+    position = models.CharField(max_length=100)  
+    phone = models.CharField(max_length=15)  
+    address = models.TextField()  
+    join_date = models.DateField(auto_now_add=True)  
+    profile_picture = models.ImageField(upload_to='profiles/', blank=True)  
+  
+    def __str__(self):  
+        return f'{self.user.username} - {self.employee_id}'  
+  
+class Attendance(models.Model):  
+    employee = models.ForeignKey(EmployeeProfile, on_delete=models.CASCADE)  
+    date = models.DateField(auto_now_add=True)  
+    time_in = models.TimeField()  
+    time_out = models.TimeField(null=True, blank=True)  
+    hours_worked = models.DecimalField(max_digits=5, decimal_places=2, default=0)  
+  
+    def __str__(self):  
+        return f'{self.employee} - {self.date}'  
